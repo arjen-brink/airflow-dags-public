30ebc88cf6c30fe1cbfe9bf7d26d504bb82bced3
arjen-brink <arjen@digital-strategies.nl>
2025-07-22 23:09:16 +0200

    Update hello.py

--- dags/hello.py
+++ dags/hello.py
@@ -5,5 +5,9 @@ from datetime import datetime
 def hello():
     print("👋 Hello from Airflow!")
 
-with DAG("hello_world", start_date=datetime(2024,1,1), schedule_interval="@daily", catchup=False) as dag:
+from airflow.models.dag import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+with DAG("hello_world", start_date=datetime(2024,1,1), schedule="@daily", catchup=False) as dag:
     PythonOperator(task_id="say_hello", python_callable=hello)
